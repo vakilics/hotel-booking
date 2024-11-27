b1956ba6913c5da68068a5ae950e3c0295ff6c66
Vakili, Abdul Rahman <vakili@univention.de>
2024-11-27 14:41:08 +0100

    chore: try Calss variable and instance variable!

---
+++ main2.py
@@ -0,0 +1,50 @@
+import pandas
+
+df = pandas.read_csv("hotels.csv", dtype={"id": str})
+
+
+class Hotel:
+    watermark = "This Real State Company"  # Class Variable! 
+    def __init__(self, hotel_id):
+        self.hotel_id = hotel_id
+        self.name = df.loc[df["id"] == self.hotel_id, "name"].squeeze()
+
+    def book(self):
+        """Book a hotel by changing its availability to no"""
+        df.loc[df["id"] == self.hotel_id, "available"] = "no"
+        df.to_csv("hotels.csv", index=False)
+
+    def available(self):
+        """Check if the hotel is available"""
+        availability = df.loc[df["id"] == self.hotel_id, "available"].squeeze()
+        if availability == "yes":
+            return True
+        else:
+            return False
+
+
+class ReservationTicket:
+    def __init__(self, customer_name, hotel_object):
+        self.customer_name = customer_name
+        self.hotel = hotel_object
+
+    def generate(self):
+        content = f"""
+        Thank you for your reservation!
+        Here are you booking data:
+        Name: {self.customer_name}
+        Hotel name: {self.hotel.name}
+        """
+        return content
+
+hotel1 = Hotel(hotel_id="188")
+hotel2 = Hotel(hotel_id="134")
+
+print(hotel1.name)
+print(hotel2.name)
+
+print(hotel1.watermark)
+print(Hotel.watermark)
+
+#print(Hotel.name)  # Not working! Hotel has no variable neme but its method has!
+
